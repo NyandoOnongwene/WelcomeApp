d5ea6cda3c14061925d6bb400e1b91778749a383
Nyando Onongwene <nyandoonotex@gmail.com>
2024-02-01 23:22:02 +0100

    Added images to the fxml file and completeted the exercise

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module com.example.welcomeapp {
     requires javafx.controls;
     requires javafx.fxml;
-            
-            requires com.dlsc.formsfx;
-                        
+
+    requires com.dlsc.formsfx;
+
     opens com.example.welcomeapp to javafx.fxml;
     exports com.example.welcomeapp;
 }
